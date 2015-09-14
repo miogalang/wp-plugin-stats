d5da5de51563a3897b47967bb4956bb4b6e21dca
Mio Miguel Galang <mgalang@zendesk.com>
2015-09-14 15:54:26 +0800

    Add additional stats and remove sample pages.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# wp-plugin-stats
-Simple page to show plugin stats
-
-
 Check out http://shopify.github.com/dashing for more information.
--- jobs/wordpress_stats.rb
+++ jobs/wordpress_stats.rb
@@ -2,14 +2,42 @@ require "net/http"
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
   slugs = ['zendesk', 'zopim-live-chat', 'inbox-by-zendesk']
+
   slugs.each do |slug|
     # Total Downloads
-    url = "http://api.wordpress.org/plugins/info/1.0/" + slug + ".json"
-    uri = URI(url)
+    uri = URI("http://api.wordpress.org/plugins/info/1.0/" + slug + ".json")
+    res = Net::HTTP.get_response(uri)
+    response = JSON.parse res.body
+    totalDownloads = response["downloaded"]
 
+    send_event('downloads-' + slug, {value: totalDownloads})
+
+    # Version Stats
+    uri = URI("https://api.wordpress.org/stats/plugin/1.0/" + slug)
     res = Net::HTTP.get_response(uri)
     response = JSON.parse res.body
 
-    send_event('downloads-' + slug, {value: response["downloaded"]})
+    puts slug
+    if !response.empty?
+      versionsData = []
+      response.each do |version, percentage|
+        downloadCount = percentage * totalDownloads / 100
+        versionsData << {label: version, value: downloadCount.round(0)}
+      end
+      puts versionsData
+      send_event("version-downloads-" + slug, items: versionsData)
+    end
+
+    # Download Stats
+    uri = URI("https://api.wordpress.org/stats/plugin/1.0/downloads.php?limit=30&slug=" + slug)
+    res = Net::HTTP.get_response(uri)
+    response = JSON.parse res.body
+    downloadsData = []
+    sum = 0
+    response.each_with_index do |(date, downloadsCount), index|
+      sum += downloadsCount.to_i
+      downloadsData << {x: index, y: downloadsCount}
+    end
+    send_event("daily-downloads-" + slug, points: downloadsData, displayedValue: sum/30)
   end
 end
